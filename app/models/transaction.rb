93dfd95988a8bcb9a2ca5455408f60f2f24cd249
arnie b <arnold.bwaila@gmail.com>
2020-07-24 16:26:28 +0000

    user model: added password validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,4 @@
 class Transaction<ActiveRecord::Base
   belongs_to :item 
   belongs_to :user
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User<ActiveRecord::Base 
   has_many :transactions
   has_many :items, through: :transactions
+  validates_presence_of :username, :name, :category
+  validates :password, :presence => true,
+                     :length => { minimum: 6 },
+                     :if => lambda{ new_record? || !password.nil? }
   has_secure_password
 end
