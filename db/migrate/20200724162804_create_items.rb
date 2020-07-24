4ffb1ad5d2fe81f0a2d0a955959cdebb797f4b43
arnie b <arnold.bwaila@gmail.com>
2020-07-24 16:33:47 +0000

    create_items migration: created table for items

--- db/migrate/20200724162804_create_items.rb
+++ db/migrate/20200724162804_create_items.rb
@@ -1,4 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
+    create_table :items do |t|
+      t.string :name 
+      t.string :description
+      t.integer :quantity
+      t.integer :restock_level
+    end
   end
 end
