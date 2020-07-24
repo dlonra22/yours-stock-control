088aa71d34c9e9bfaaf3b267d797133ab2eb9433
arnie b <arnold.bwaila@gmail.com>
2020-07-24 18:20:22 +0000

    seeds: added 2 users to seed file

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+#users 1 of type admin and 1 of type teller
+jayz=User.create(username: "jayz", name: "Sean Carter", password: "Hova123", category: "Admin")
+lewis=User.create(username: "lewis" name: "Lewis Hamilton" password: "HammerTime" category: "Teller")
+
+#items
