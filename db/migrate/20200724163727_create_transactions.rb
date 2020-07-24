051e08f5549fc095d592e83ab54c5efb6cdfecf8
arnie b <arnold.bwaila@gmail.com>
2020-07-24 16:58:22 +0000

    create_transactions: added transactions table

--- db/migrate/20200724163727_create_transactions.rb
+++ db/migrate/20200724163727_create_transactions.rb
@@ -1,4 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
+    create_table :transactions do |t|
+      t.integer :user_id 
+      t.integer :item_id
+      t.string :type 
+      t.timestamps
   end
 end
