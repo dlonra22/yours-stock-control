7f22393823bfa608a66cb33802d581c735d3ad55
arnie b <arnold.bwaila@gmail.com>
2020-07-25 15:50:06 +0000

    commented out all other controller actions apart from main controller in order to test main controller function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,36 +2,36 @@ class UsersController < ApplicationController
 
   # GET: /users
   get "/users" do
-    erb :"/users/index.html"
+    #erb :"/users/index.html"
   end
 
   # GET: /users/new
   get "/users/new" do
-    erb :"/users/new.html"
+   # erb :"/users/new.html"
   end
 
   # POST: /users
   post "/users" do
-    redirect "/users"
+    #redirect "/users"
   end
 
   # GET: /users/5
   get "/users/:id" do
-    erb :"/users/show.html"
+   # erb :"/users/show.html"
   end
 
   # GET: /users/5/edit
   get "/users/:id/edit" do
-    erb :"/users/edit.html"
+   # erb :"/users/edit.html"
   end
 
   # PATCH: /users/5
   patch "/users/:id" do
-    redirect "/users/:id"
+    #redirect "/users/:id"
   end
 
   # DELETE: /users/5/delete
   delete "/users/:id/delete" do
-    redirect "/users"
+   # redirect "/users"
   end
 end
