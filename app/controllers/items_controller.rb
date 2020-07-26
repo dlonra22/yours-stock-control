6a5ace11e535cb9503ae5f10d5fe934afcee9bad
arnie b <arnold.bwaila@gmail.com>
2020-07-26 13:04:34 +0000

    rectified routes for items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,38 +1,38 @@
 class ItemsController < ApplicationController
 
   # GET: /users/items
-  get "users/items" do
+  get "users/:id/items" do
     #erb :"users/items/index.html"
   end
 
 
   # GET: /users/items/new only for admins
-  get "users/items/new" do
+  get "users/:id/items/new" do
    # erb :"/items/new.html"
   end
 
   # POST: users/items
-  post "users/items" do
+  post "users/:id/items" do
    # redirect "/items"
   end
 
   # GET: users/items/5
-  get "users/items/:id" do
+  get "users/:id/items/:iid" do
    # erb :"/items/show.html"
   end
 
   # GET: users/items/5/edit
-  get "users/items/:id/edit" do
+  get "users/:id/items/:iid/edit" do
    # erb :"/items/edit.html"
   end
 
   # PATCH: users/items/5
-  patch "users/items/:id" do
+  patch "users/:id/items/:iid" do
    # redirect "/items/:id"
   end
 
-  # DELETE: users/items/5/delete only admins
-  delete "users/items/:id/delete" do
+  # DELETE: delete only admins redirects to items home page
+  delete "users/:id/items/:iid/delete" do
    # redirect "/items"
   end
 end
