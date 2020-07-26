d7c64c0cab4b4cef2d59e2949ce4b5e0149ef74a
arnie b <arnold.bwaila@gmail.com>
2020-07-26 15:05:50 +0000

    updated validations for password field to to include password confirmation and message on error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User<ActiveRecord::Base
   has_many :items, through: :transactions
   validates_presence_of :username, :name
   validates_uniqueness_of :username
+  validates_confirmation_of :password,  message: 'Passwords Should Match'
+  validates_presence_of :password_confirmation, if: :password_changed?
+
   validates :password, :presence => true,
                      :length => { minimum: 6 },
                      :if => lambda{ new_record? || !password.nil? }
