0c87f5edaaa55b16d4da0da99bbcdd7bc485eed2
arnie b <arnold.bwaila@gmail.com>
2020-07-27 10:10:44 +0000

    rectified register and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "users/#{user.id}/home"
+      redirect "users/#{user.id}"
     
     else
       #show error message
@@ -28,24 +28,30 @@ class UsersController < ApplicationController
     end 
   end
   
+  get "users/:id" do 
+    erb "users/home"
+  end
+  
+  
+  
   get "/register" do
   #check is user is already logged in, if so redirect to users/new
    if User.all.empty?
       @appregister = true #sends a status check to the register true = first registration of app/ no users in database
       erb:register 
    else 
-     erb :notadmin
      redirect "/login"
    end
   end
   
    post "/register" do
      "Hey you registered"
+     User.create(params)
+     session[:user_id] = user.id
+     redirect "users/#{user.id}"
    end
   
-  get "users/:id/home" do 
-  end
-  
+
   
   
   get "/users" do #only admins
