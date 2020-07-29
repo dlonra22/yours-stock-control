8093b060f66d6644c0eb580a7cc1b9f49f799036
arnie b <arnold.bwaila@gmail.com>
2020-07-29 12:29:10 +0000

    tested patch on editmyprofile...removed uniqueness validations to test other functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User<ActiveRecord::Base
   has_many :transactions
   has_many :items, through: :transactions
   validates_presence_of :username, :name
-  validates_uniqueness_of :username
-  validates_confirmation_of :password,  message: 'Passwords Should Match'
-  validates_presence_of :password_confirmation, if: :password_digest_changed?
+  #validates_uniqueness_of :username
+  #validates_confirmation_of :password,  message: 'Passwords Should Match'
+  #validates_presence_of :password_confirmation, if: :password_digest_changed?
 
   validates :password, :presence => true,
                      :length => { minimum: 6 },
