dee22a5431915c58d63aec2d412797ec6676ddb5
arnie b <arnold.bwaila@gmail.com>
2020-07-30 14:00:05 +0000

    newitem.erb updated title and added back to items button items controller removed unimplemented routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,36 +5,12 @@ class ItemsController < ApplicationController
     erb :"items/allitems"
   end
   
+  get "/items/new" do
+   erb :"items/newitem"
+  end
+  
   get "/items/:id" do 
     erb :"items/showitem"
   end
 
-  get "items/new" do
-   erb :"items/newitem"
-  end
-
-  # POST: users/items
-  post "user/:id/items" do
-   # redirect "/items"
-  end
-
-  # GET: users/items/5
-  get "user/:id/items/:iid" do
-   # erb :"/items/show.html"
-  end
-
-  # GET: users/items/5/edit
-  get "user/:id/items/:iid/edit" do
-   # erb :"/items/edit.html"
-  end
-
-  # PATCH: users/items/5
-  patch "user/:id/items/:iid" do
-   # redirect "/items/:id"
-  end
-
-  # DELETE: delete only admins redirects to items home page
-  delete "users/:id/items/:iid/delete" do
-   # redirect "/items"
-  end
 end
