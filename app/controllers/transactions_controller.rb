65bd715f609725edc2957a716c0d7e573cdeab53
arnie b <arnold.bwaila@gmail.com>
2020-07-30 14:20:43 +0000

    transactions controller - completed create new transaction route

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -30,7 +30,23 @@ get "/mytransactions" do
   end #end of get alltransactions
   
   get "/transactions/new" do 
-    erb :"transactions/new"
+    if logged_in?
+       @user = User.find_by(id: current_user.id)
+       items = Items.all
+       @items_available = [] 
+       items.each do |item| 
+         @items_available << item if item.quantity > 0
+       end
+       if @items_available.count < 1 
+         #no items in inventory 
+         redirect "/users/#{current_user.id}"
+       else
+          erb :"transactions/new"
+        end
+    else
+      #please login 
+      redirect"/login"
+    end
   end
   
 end #end of controller
