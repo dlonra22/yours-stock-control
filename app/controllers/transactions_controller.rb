cf430037b1187d70cd4fc00a3d041ebea7420aeb
arnie b <arnold.bwaila@gmail.com>
2020-07-30 12:44:11 +0000

    transactions controller - updated get routes for transactions views to correctly render erb files

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,29 +2,31 @@ class TransactionsController < ApplicationController
 
 get "/mytransactions" do
     if logged_in?
-      @transactions = Transaction.all
+      transactions = Transaction.all
       @user_trans = [] 
-      @transactions.each do |t| 
+      transactions.each do |t| 
         @user_trans << t if t.user_id == current_user.id
       end
-      erb :mytransactions
+      erb :"transactions/mytransactions"
     else 
       # please login 
       redirect "/login"
     end
-  end
+  end #end of get mytransactions
   
   get "/alltransactions" do
     if logged_in?
-      @transactions = Transaction.all
-      @user_trans = [] 
-      @transactions.each do |t| 
-        @user_trans << t if t.user_id == current_user.id
+      if current_user.is_admin
+        @transactions = Transaction.all
+        erb :"transactions/alltransactions"
+      else 
+        #only admins 
+        redirect "/users/#{current_user.id}"
       end
-      erb :alltransactions
     else 
       # please login 
       redirect "/login"
     end
-  end
-end
+  end #end of get alltransactions
+  
+end #end of controller
