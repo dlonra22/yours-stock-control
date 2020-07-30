05fd6060845256bc2bffdf2b4e3b153a43f08e5d
arnie b <arnold.bwaila@gmail.com>
2020-07-30 12:38:00 +0000

    transactions_controller.rb - created routes to get users transactions and all transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,37 +1,30 @@
 class TransactionsController < ApplicationController
 
-  # GET: users/transactions
-  get "user/:id/transactions" do
-   # erb :"users/transactions/index.html"
+get "/mytransactions" do
+    if logged_in?
+      @transactions = Transaction.all
+      @user_trans = [] 
+      @transactions.each do |t| 
+        @user_trans << t if t.user_id == current_user.id
+      end
+      erb :mytransactions
+    else 
+      # please login 
+      redirect "/login"
+    end
   end
-
-  # GET: users/transactions/new
-  get "user/:id/transactions/new" do
-   # erb :"/transactions/new.html"
+  
+  get "/alltransactions" do
+    if logged_in?
+      @transactions = Transaction.all
+      @user_trans = [] 
+      @transactions.each do |t| 
+        @user_trans << t if t.user_id == current_user.id
+      end
+      erb :alltransactions
+    else 
+      # please login 
+      redirect "/login"
+    end
   end
-
-  # POST: users/transactions
-  post "user/:id/transactions" do
-  #  redirect "/transactions"
-  end
-
-  # GET: transactions show page
-  get "user/:id/transactions/:tid" do
- #   erb :"/transactions/show.html"
-  end
-
-  # GET: /transactions/5/edit should not be enable to do the below.
-  #get "/transactions/:id/edit" do
-  #  erb :"/transactions/edit.html"
- # end
-
-  # PATCH: /transactions/5
-  #patch "/transactions/:id" do
-  #  redirect "/transactions/:id"
- # end
-
-  # DELETE: /transactions/5/delete
-  #delete "/transactions/:id/delete" do
-  #  redirect "/transactions"
-  #end
 end
