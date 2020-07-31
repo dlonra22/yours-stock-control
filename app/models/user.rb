a34573a225101787766f1d896489986bb8856937
arnie b <arnold.bwaila@gmail.com>
2020-07-31 19:03:21 +0000

    removed duplicated validation for password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User<ActiveRecord::Base
   has_many :items, through: :transactions
   validates_presence_of :username, :name
   validates_uniqueness_of :username
-  validates_confirmation_of :password,  message: 'Passwords Should Match'
   validates_presence_of :password_confirmation, if: :password_digest_changed?
 
   validates :password, :presence => true,
