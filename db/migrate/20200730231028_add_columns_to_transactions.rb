f9e0adc81e3cd2c408e912902de2e4ebae3f888e
arnie b <arnold.bwaila@gmail.com>
2020-07-31 01:58:07 +0000

    changed attribute on all transactions views to from value to tr_value

--- db/migrate/20200730231028_add_columns_to_transactions.rb
+++ db/migrate/20200730231028_add_columns_to_transactions.rb
@@ -1,6 +1,6 @@
 class AddColumnsToTransactions < ActiveRecord::Migration
   def change
-    add_column :transactions, :value, :float 
+    add_column :transactions, :tr_value, :float 
     add_column :transactions, :transaction_notes, :text
   end
 end
